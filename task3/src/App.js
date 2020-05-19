13f6207f39e0272182e8c6bea3551daf3c279272
sadia <sadia@techhivebd.com>
2020-05-20 02:19:33 +0600

    initialize task3 boilerplate

---
+++ task3/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header'
+import Footer from './components/Footer'
+// import Home from './pages/Home'
+
+
+function App() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
