ef21cbbbdda2ced25c7f9b7eec516348255060be
sadia <sadia@techhivebd.com>
2020-05-20 00:45:21 +0600

    add footer

--- task2/src/App.js
+++ task2/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Home from './pages/Home'
 
 function App() {
   return (
-    <div style={{width:'100%'}}>
+    <div style={{ width: '100%' }}>
       <Header />
       <Home />
+      <Footer/>
     </div>
   );
 }
---
+++ task2/src/components/Footer/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      <footer class="page-footer font-small unique-color-dark pt-4" style={{ background: '#343a40' }}>
+        <div class="container">
+          <ul class="list-unstyled list-inline text-center py-2">
+            <li class="list-inline-item">
+            </li>
+            <li class="list-inline-item">
+            </li>
+          </ul>
+
+        </div>
+
+        <div class="footer-copyright text-center py-3">© 2020 Copyright:
+        Movies.com
+        </div>
+
+      </footer>
+
+    </div>
+  )
+}
+
+export default Footer
